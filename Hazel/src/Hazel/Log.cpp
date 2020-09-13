39472a0706a4f8b6dfa09054b94855a9665af73a
Gabriel Kwong <gabrielkwong971@gmail.com>
2020-09-12 16:30:56 -0800

    Event System

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -14,7 +14,7 @@ namespace Hazel {
 		s_CoreLogger->set_level(spdlog::level::trace);
 		
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 
 }
