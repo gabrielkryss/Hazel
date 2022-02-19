6c9bcfb132cb42a7739c4ab4e9a6af5d267ece40
gabrielkryss <gabrielkwong971@gmail.com>
2022-02-18 21:01:44 -0800

    Window Events and Layers

--- Hazel/src/Hazel/Events/ApplicationEvent.h
+++ Hazel/src/Hazel/Events/ApplicationEvent.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Event.h"
+#include "hzpch.h"
 
 namespace Hazel {
 
--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "Hazel/Core.h"
-#include <string>
-#include <functional>
+#include "string"
+#include "functional"
 
 namespace Hazel {
 
@@ -40,6 +40,8 @@ namespace Hazel {
 	{
 		friend class EventDispatcher;
 	public:
+		bool Handled = false;
+
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -49,8 +51,6 @@ namespace Hazel {
 		{
 			return GetCategoryFlags() & category;
 		}
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher
@@ -68,7 +68,7 @@ namespace Hazel {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 			return false;
