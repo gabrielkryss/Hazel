2ae123facbaac01c5b08bed98c5667e57db63df9
gabrielkryss <gabrielkwong971@gmail.com>
2022-02-19 23:59:50 -0800

    OpenGL (GLAD) - brought in GLAD with premake file

--- Hazel/src/Platform/WindowsWindow.cpp
+++ Hazel/src/Platform/WindowsWindow.cpp
@@ -5,6 +5,9 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
+#include "glad/glad.h"
+#include <GLFW/glfw3.h> // <--- this has to be included after Glad, don't know why, cuz Glad included it already? which causes and error. Tried using GLFW_INCLUDE_NONE in Hazel premake file but didn't work.
+
 namespace Hazel {
 
 	static bool s_GLFWInitialized = false;
@@ -48,6 +51,8 @@ namespace Hazel {
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
